35e4e94e4a9614e1077842146975debe0c7048ca
Tim van Steenbergen <tim@tieka.nl>
2015-08-04 13:06:11 +0200

    H10 not complete yet

--- ch10p2Callbacks.php
+++ ch10p2Callbacks.php
@@ -19,6 +19,7 @@ usort($array, $callback);
 CODE
 );
 echo '<h3>Listing 10.9: Using a class as a callback</h3>';
+//@todo sorter needs no (); usort wants 2 params
 showcode(<<<'CODE'
 class Sorter()
 {
@@ -30,9 +31,34 @@ $sorter = new Sorter();
 usort($sorter);
 CODE
 );
-echo '<h3></h3>';
+echo '<h3>Listing 10.10: Userland callbacks</h3>';
+//@todo Added two ';' and some variables
 showcode(<<<'CODE'
-
+function myFunction(){}
+Class SomeOtherClass
+{
+   public function method(){}
+}
+$obj = new SomeOtherClass;
+$array = [1,2,3,5,4];// Variable Functions
+$callback = "myFunction";
+$callback();
+// object method call:
+$callback = [$obj, 'method']; // $obj->method() callback
+$callback();
+// or static method:
+$callback = ['SomeOtherClass', 'method']; // SomeOtherClass::method()
+$callback();
+// Closures
+$callback = function() { };
+$callback();
+// Objects with Invoke magic method:
+class invokeCallback
+{
+public function __invoke() { }
+}
+$callback = new invokeCallback();
+$callback();
 CODE
 );
 echo '<h3></h3>';
